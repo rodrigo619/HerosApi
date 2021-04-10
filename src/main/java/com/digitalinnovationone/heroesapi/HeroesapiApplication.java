1d8b85a1fab952dd5f9c5c4b9d6fc00e5a64b983
Rodrigo C A Pacheco <rodrigocarvalho619@gmail.com>
2021-04-10 15:06:15 -0300

    Project of Heroes Storage

---
+++ src/main/java/com/digitalinnovationone/heroesapi/HeroesapiApplication.java
@@ -0,0 +1,16 @@
+package com.digitalinnovationone.heroesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+
+@SpringBootApplication
+@EnableDynamoDBRepositories
+public class HeroesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesapiApplication.class, args);
+		System.out.println("Programa Super-Heróis com Webflux");
+	}
+
+}
